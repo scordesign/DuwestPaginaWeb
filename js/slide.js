b61c67a2149fe02668980b30e4ce275d82ae5d90
Seb1995 <99344424+scordesign@users.noreply.github.com>
2025-02-05 15:23:06 -0500

    Update slide.js

--- js/slide.js
+++ js/slide.js
@@ -2,8 +2,8 @@ const images = [
     'images/image1.jpg',
     'images/image2.jpg',
     'images/image3.jpg',
-    'images/image4.jpg',
-    'images/image5.jpg',
+    // 'images/image4.jpg',
+    // 'images/image5.jpg',
     
     // Añade tantas imágenes como necesites
 ];
