079a3165c31199788c81ea0bc91d721c28b6b5d1
Seb1995 <99344424+scordesign@users.noreply.github.com>
2025-07-08 09:14:55 -0500

    Test ENV

--- aplication/RequestController.php
+++ aplication/RequestController.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-
+require_once __DIR__ . '/../vendor/autoload.php';
 require_once ('controller/ProductsController.php');
 require_once ('controller/UserController.php');
 require_once ('controller/FilterController.php');
